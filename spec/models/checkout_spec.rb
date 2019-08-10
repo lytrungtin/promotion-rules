4e11d237a7aa9351646508d8d80390ec25a3e1ab
LyTrungTin <trungtin.sotech@gmail.com>
2019-08-11 03:08:53 +0700

    Update context and position tests.

--- spec/models/checkout_spec.rb
+++ spec/models/checkout_spec.rb
@@ -4,24 +4,17 @@ require_relative '../../app/models/checkout.rb'
 
 describe Checkout do
   before do
-    / If you buy 2 or more lavender hearts then the price drops to £8.50 /
-    @item1 = Product.new(product_code: '001', name: 'Lavender heart',
-                         price: 9.25, quantity_discount: 2, price_discount: 8.5)
-
-    @item2 = Product.new(product_code: '002', name: 'Personalised cufflinks', price: 45.00)
-    @item3 = Product.new(product_code: '003', name: 'Kids T-shirt', price: 19.95)
-
-    / If you spend over £60, then you get 10% off of your purchase /
     @promotional_rules = Promotion.new(min_total_price: 61, discount_rate: 10)
     @co = Checkout.new(@promotional_rules)
   end
 
-  describe 'scan' do
+  describe '#scan' do
     context 'When items have price discount from product' do
       before do
         @item_has_discount = Product.new(price: 20.75, quantity_discount: 3, price_discount: 15.5)
         @i = 0
       end
+
       context 'When total checkout less than min total price discount' do
         context 'When buy items is not enough quantity discount' do
           before do
@@ -37,6 +30,7 @@ describe Checkout do
             expect(@co.total).to eq price_from_item_discount
           end
         end
+
         context 'When buy items is enough quantity discount' do
           before do
             @quantity = @item_has_discount.quantity_discount
@@ -58,6 +52,7 @@ describe Checkout do
           before do
             @quantity = @item_has_discount.quantity_discount - 1
           end
+
           it 'It should be not applied discount from product but applied from promotion' do
             begin
               @co.scan(@item_has_discount)
@@ -72,10 +67,12 @@ describe Checkout do
             expect(@co.total).to eq total_expected
           end
         end
+
         context 'When buy items is enough quantity discount' do
           before do
             @quantity = @item_has_discount.quantity_discount
           end
+
           it 'It should be applied discount from both product and promotion' do
             begin
               @co.scan(@item_has_discount)
@@ -92,52 +89,61 @@ describe Checkout do
         end
       end
     end
+  end
+
+  describe 'Integrate sample tests' do
+    before do
+      / If you buy 2 or more lavender hearts then the price drops to £8.50 /
+      @item_001 = Product.new(product_code: '001', name: 'Lavender heart',
+                           price: 9.25, quantity_discount: 2, price_discount: 8.5)
+
+      @item_002 = Product.new(product_code: '002', name: 'Personalised cufflinks', price: 45.00)
+      @item_003 = Product.new(product_code: '003', name: 'Kids T-shirt', price: 19.95)
+
+      / If you spend over £60, then you get 10% off of your purchase /
+    end
 
     context 'If you buy 2 or more lavender hearts then the price drops to £8.50' do
       it 'It should be applied discount from product' do
-        co = Checkout.new(@promotional_rules)
-        co.scan(@item1)
-        co.scan(@item1)
-        expect(co.total).to eq @item1.price_discount * 2
+        @co.scan(@item_001)
+        @co.scan(@item_001)
+        expect(@co.total).to eq @item_001.price_discount * 2
       end
     end
+
     context 'If you spend over £60, then you get 10% off of your purchase' do
       it 'It should be applied discount from promotion' do
-        co = Checkout.new(@promotional_rules)
-        co.scan(@item2)
-        co.scan(@item2)
-        expect(co.total).to eq (@item2.price * 2) * 0.9
+        @co.scan(@item_002)
+        @co.scan(@item_002)
+        expect(@co.total).to eq (@item_002.price * 2) * 0.9
       end
     end
-  end
 
-  describe 'total' do
-    it 'Test 1' do
-      co = Checkout.new(@promotional_rules)
-      co.scan(@item1)
-      co.scan(@item2)
-      co.scan(@item3)
-      price = co.total
-      expect(price).to eq 66.78
-    end
-
-    it 'Test 2' do
-      co = Checkout.new(@promotional_rules)
-      co.scan(@item1)
-      co.scan(@item3)
-      co.scan(@item1)
-      price = co.total
-      expect(price).to eq 36.95
-    end
-
-    it 'Test 3' do
-      co = Checkout.new(@promotional_rules)
-      co.scan(@item1)
-      co.scan(@item2)
-      co.scan(@item1)
-      co.scan(@item3)
-      price = co.total
-      expect(price).to eq 73.76
+    context 'When input sample test data' do
+      context 'When Basket: 001,002,003' do
+        it 'should return total price: £66.78' do
+          [@item_001, @item_002, @item_003].each do |item|
+            @co.scan(item)
+          end
+          expect(@co.total).to eq 66.78
+        end
+      end
+      context 'When Basket: 001,003,001' do
+        it 'should return total price: £36.95' do
+          [@item_001, @item_003, @item_001].each do |item|
+            @co.scan(item)
+          end
+          expect(@co.total).to eq 36.95
+        end
+      end
+      context 'When Basket: 001,002,001,003' do
+        it 'should return total price: £73.76' do
+          [@item_001, @item_002, @item_001, @item_003].each do |item|
+            @co.scan(item)
+          end
+          expect(@co.total).to eq 73.76
+        end
+      end
     end
   end
 end
